5bf4e2d879687b645762490191bd49a01ca57c54
a person <bbdred@gmail.com>
2022-03-07 15:25:25 -0600

    Update Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,1 +1,6 @@
-<h1>Hello Dad Love You</h1>
+<h1>Art Meta One Art Classes</h1>
+In person located at Madison Wisconsin, with classes ofered internationaly online.  
+Art Meta One serves individuals who want to advance their artistic talents. Our programs are geared to help artists further their skills.
+Membership Dues:
+Residents of Madison and surrounding areas:
+Associate Resident Members pay $448 per year, payable quarterly.
