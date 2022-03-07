bc1c9bd00e33a56de7e717e5946f2b94bfad5331
a person <bbdred@gmail.com>
2022-03-07 15:41:58 -0600

    Update Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,6 +1,18 @@
-<h1>Art Meta One Art Classes</h1>
-In person located at Madison Wisconsin, with classes ofered internationaly online.  
-Art Meta One serves individuals who want to advance their artistic talents. Our programs are geared to help artists further their skills.
-Membership Dues:
-Residents of Madison and surrounding areas:
-Associate Resident Members pay $448 per year, payable quarterly.
+<h1>Art Meta One Art Classes</h1> <BR>
+In person located at Madison Wisconsin, with classes ofered internationaly online.  <BR>
+Art Meta One serves individuals who want to advance their artistic talents. Our programs are geared to help artists further their skills. <BR>
+Membership Dues:<BR>
+Residents of Madison and surrounding areas:<BR>
+Associate Resident Members pay $448 per year, payable quarterly.<BR>
+
+<div class="card">
+  <img src="images/Lamp-Sol.png" alt="Art Meta One Art Classes" style="width:100%">
+  <h2>Lamp Sol NFT </h2>
+  <p class="title">Artist</p>
+  <p>Ben Banker</p>
+  <div style="margin: 24px 0;"> 
+    <a href="https://twitter.com/willis_dontabo/status/1480946056527499264"><i class="fa fa-twitter"></i></a>   
+    <a href="https://www.facebook.com/C.NJ.NCT/posts/4668122783305489"><i class="fa fa-facebook"></i></a> 
+  </div>
+  <p><button onclick="document.location='https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/74910965659129920827967912483173887147949622782888675597174626359838240669697'">Buy Now</button></p>
+</div>	
