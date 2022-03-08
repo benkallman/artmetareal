0dbb3eb7a3218e99e880322d0a689e7578abda74
a person <bbdred@gmail.com>
2022-03-07 20:05:08 -0600

    Update Classes.svelte

--- src/pages/Classes.svelte
+++ src/pages/Classes.svelte
@@ -1,4 +1,4 @@
-
+<br>
 <div class="card">
   <img src="images/benbanker.jpg" alt="Art Meta One Art Classes" style="width:100%">
   <h2>Classes located in Madison Wisconsin, with classes also offered online.</h2>
