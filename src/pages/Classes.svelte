f9e91948247656ff92b299ce7c8e84f65d201119
a person <bbdred@gmail.com>
2022-03-07 19:53:42 -0600

    Update Classes.svelte

---
+++ src/pages/Classes.svelte
@@ -0,0 +1,13 @@
+<h1> Classes <h>
+<div class="card">
+  <img src="images/benbanker.jpg" alt="Art Meta One Art Classes" style="width:100%">
+  <h2>Classes located in Madison Wisconsin, with classes also offered online.</h2>
+  <p class="title">Art Meta One serves individuals who want to advance their artistic talents. Our programs are geared to help artists further their skills. </p>
+  <p>Membership Dues:<br>
+$896 per year, payable quarterly. Or pay as you go $25 per Class.</p>
+  <div style="margin: 24px 0;"> 
+    <a href="https://twitter.com/willis_dontabo/status/1480946056527499264"><i class="fa fa-twitter"></i></a>   
+    <a href="https://www.facebook.com/C.NJ.NCT/posts/4668122783305489"><i class="fa fa-facebook"></i></a> 
+  </div>
+  <p><button onclick="document.location='https://www.artmeta.one/Classes'">Choose Course Now</button></p>
+</div>
