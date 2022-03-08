67825dc0145ecc349cee3c31f9edb35f12ecb142
a person <bbdred@gmail.com>
2022-03-08 17:36:01 -0600

    Update Classes.svelte

--- src/pages/Classes.svelte
+++ src/pages/Classes.svelte
@@ -12,7 +12,7 @@ $896 per year, payable quarterly. Or pay as you go $25 per Class.</p>
 
 </div>
 <br>
-<div> <center>
+<div><center>
   <div><div style="
   overflow: auto;
   display: flex;
@@ -24,21 +24,30 @@ $896 per year, payable quarterly. Or pay as you go $25 per Class.</p>
   border: 1px solid rgba(0, 0, 0, 0.1);
   box-shadow: -2px 10px 5px rgba(0, 0, 0, 0);
   border-radius: 10px;
-  font-family: SQ Market, Helvetica, Arial, sans-serif;
+  font-family: SQ Market, SQ Market, Helvetica, Arial, sans-serif;
   ">
   <div style="padding: 20px;">
-  <a target="_blank" data-url="https://square.link/u/YgbNBYll?src=embd" href="https://square.link/u/YgbNBYll?src=embed" style="
+  <p style="
+    font-size: 18px;
+    line-height: 20px;
+  ">Year of Weekly Classes</p>
+  <p style="
+    font-size: 18px;
+    line-height: 20px;
+    font-weight: 600;
+  ">$896.00</p>
+  <a target="_blank" data-url="https://square.link/u/9vWbXIpH?src=embd" href="https://square.link/u/9vWbXIpH?src=embed" style="
     display: inline-block;
     font-size: 18px;
     line-height: 48px;
     height: 48px;
     color: #ffffff;
     min-width: 212px;
-    background-color: #000000;
+    background-color: #006aff;
     text-align: center;
     box-shadow: 0 0 0 1px rgba(0,0,0,.1) inset;
-    
-  ">Pay Now One Year</a>
+    border-radius: 0px;
+  ">Pay now</a>
   </div>
 </div>
 </div>
