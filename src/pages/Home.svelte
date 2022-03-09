570e772eb377d572ad1ff6e7dd5bc89748c458e8
a person <bbdred@gmail.com>
2022-03-08 21:41:08 -0600

    Update Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -4,7 +4,7 @@
   <h2>Classes located in Madison Wisconsin, with classes also offered online.</h2>
   <p class="title">Art Meta One serves individuals who want to advance their artistic talents. Our programs are geared to help artists further their skills. </p>
   <p>Membership Dues:<br>
-$896 per year, payable quarterly. Or pay as you go $25 per Class.</p>
+$896 per year, payable quarterly. Recurring weekly hourly courses $20. Or pay as you go $30 per Class.</p>
   <div style="margin: 24px 0;"> 
     <a href="https://twitter.com/ArtMeta1"><i class="fa fa-twitter"></i></a>   
     <a href="https://www.facebook.com/artmeta.one/"><i class="fa fa-facebook"></i></a> 
