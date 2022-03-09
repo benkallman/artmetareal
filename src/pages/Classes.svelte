4a3e85f625fa5fadf353ceb7b24945981f407e45
a person <bbdred@gmail.com>
2022-03-08 20:59:59 -0600

    Update Classes.svelte

--- src/pages/Classes.svelte
+++ src/pages/Classes.svelte
@@ -6,7 +6,7 @@
   <p>Membership Dues:<br>
 $896 per year, payable quarterly. Recurring weekly hourly courses $20. Or pay as you go $30 per Class.</p>
   <div style="margin: 24px 0;"> 
-    <a href="https://twitter.com/willis_dontabo/status/1480946056527499264"><i class="fa fa-twitter"></i></a>   
+    <a href="https://twitter.com/ArtMeta1"><i class="fa fa-twitter"></i></a>   
     <a href="https://www.facebook.com/C.NJ.NCT/posts/4668122783305489"><i class="fa fa-facebook"></i></a> 
   </div>
 
