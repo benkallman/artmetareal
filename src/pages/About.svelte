24272ff05980a598ef1e3f741564b3ac7c7d137e
a person <bbdred@gmail.com>
2022-03-09 22:20:09 -0600

    Update About.svelte

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -4,7 +4,7 @@
   <img src="images/benbanker.jpg" alt="Art Meta One Art Classes" style="width:100%">
   <h2>Class located in Madison Wisconsin, with classes also offered online. Taught by Benjamin Kallman.</h2>
   <p class="title">Drawing and Painting. <br>Brush, pen, or digital. Guided classes either structured by students 
-    interest or standard class to find the students interest. </p>
+    interest or standard class to find the students interest.</p>
   <p>Instructor: Benjamin Kallman<br>
 Ben is an artist based in the midwest with a Bachelor’s degree in Fine Arts from The School of
 the Art Institute of Chicago. He has been painting for 25 years.</p>
