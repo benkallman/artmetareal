1d048384dd272681efee98ce8bf78fbc95a68c8b
orioncrespo10 <96392837+orioncrespo10@users.noreply.github.com>
2022-04-11 19:13:53 -0500

    Update Teachers.svelte

--- src/pages/Teachers.svelte
+++ src/pages/Teachers.svelte
@@ -5,37 +5,37 @@
 <div class="container">
   <div class="mySlides">
     <div class="numbertext">1 / 6</div>
-    <img src="images/benbenker.jpg" style="width:100%">
+    <img src="images/benbenker.jpg" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">2 / 6</div>
-    <img src="images/close.jpg" style="width:100%">
+    <img src="images/close.jpg" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">3 / 6</div>
-    <img src="images/closelamp.jpg" style="width:100%">
+    <img src="images/closelamp.jpg" style="width:50%">
   </div>
     
   <div class="mySlides">
     <div class="numbertext">4 / 6</div>
-    <img src="images/dadart.jpg" style="width:100%">
+    <img src="images/dadart.jpg" style="width:50%">
   </div>
 
   <div class="mySlides">
     <div class="numbertext">5 / 6</div>
-    <img src="images/dragon.jpg" style="width:100%">
+    <img src="images/dragon.jpg" style="width:50%">
   </div>
     
   <div class="mySlides">
     <div class="numbertext">5 / 6</div>
-    <img src="images/lamp.jpg" style="width:100%">
+    <img src="images/lamp.jpg" style="width:50%">
   </div>
   
   <div class="mySlides">
     <div class="numbertext">5 / 6</div>
-    <img src="images/ninja.jpg" style="width:100%">
+    <img src="images/ninja.jpg" style="width:50%">
   </div>
 
     
@@ -48,25 +48,25 @@
 
   <div class="row">
     <div class="column">
-      <img class="demo cursor" src="images/benbenker.jpg" style="width:50%" onclick="currentSlide(1)" alt="The Woods">
+      <img class="demo cursor" src="images/benbenker.jpg" style="width:25%" onclick="currentSlide(1)" alt="benbenker">
     </div>
     <div class="column">
-      <img class="demo cursor" src="images/close.jpg" style="width:50%" onclick="currentSlide(2)" alt="Cinque Terre">
+      <img class="demo cursor" src="images/close.jpg" style="width: 25%" onclick="currentSlide(2)" alt="close">
     </div>
     <div class="column">
-      <img class="demo cursor" src="images/closelamp.jpg" style="width:50%" onclick="currentSlide(3)" alt="Mountains and fjords">
+      <img class="demo cursor" src="images/closelamp.jpg" style="width:25%" onclick="currentSlide(3)" alt="closelamp">
     </div>
     <div class="column">
-      <img class="demo cursor" src="images/dadart.jpg" style="width:50%" onclick="currentSlide(4)" alt="Northern Lights">
+      <img class="demo cursor" src="images/dadart.jpg" style="width:25%" onclick="currentSlide(4)" alt="dadart">
     </div>
     <div class="column">
-      <img class="demo cursor" src="images/dragon.jpg" style="width:50%" onclick="currentSlide(5)" alt="Nature and sunrise">
+      <img class="demo cursor" src="images/dragon.jpg" style="width:25%" onclick="currentSlide(5)" alt="dragon">
     </div>    
     <div class="column">
-      <img class="demo cursor" src="images/lamp.jpg" style="width:50%" onclick="currentSlide(6)" alt="Snowy Mountains">
+      <img class="demo cursor" src="images/lamp.jpg" style="width:25%" onclick="currentSlide(6)" alt="lamp">
     </div>
     <div class="column">
-      <img class="demo cursor" src="images/ninja.jpg" style="width:50%" onclick="currentSlide(6)" alt="Snowy Mountains">
+      <img class="demo cursor" src="images/ninja.jpg" style="width:25%" onclick="currentSlide(6)" alt="ninja">
     </div>
   </div>
 </div>
