f71538e4749c848151c3f0d809b5eb5cbb23571d
orioncrespo10 <96392837+orioncrespo10@users.noreply.github.com>
2022-04-14 15:55:55 -0500

    Update Teachers.svelte

--- src/pages/Teachers.svelte
+++ src/pages/Teachers.svelte
@@ -1,28 +1,57 @@
+<html>
+<head>
+<style>
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
 
-<link rel="stylesheet" href='./style.css">
-<script src="https://use.fontawesome.com/09f10751ad.js"></script>
+div.gallery:hover {
+  border: 1px solid #777;
+}
 
-                             
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
+</head>
 <body>
-<div class="carousel-container">
-                             
-         
-          <i class="fa fa-arrow-left" aria-hidden="true"></i>
-          <i class="fa fa-arrow-right" aria-hidden="true"></i>
 
-         <div class="carousel-slide">
-             <img src="images/lamp.jpg" id="lastClone alt=""> 
-             <img src="images/dragon.jpg" alt="">                
-             <img src="images/dadart(1).jpg" alt="">                     
-             <img src="images/ninja.jpg" alt="">                      
-             <img src="images/closelamp.jpg" alt="">  
-             <img src="images/lamp.jpg" alt="">  
-             <img src="images/dragon.jpg" id="fistClone alt="">                  
-           </div>
-        </div>
-                             
-   <button id="prevBtn">Prev</button>
-   <button id="nextBtn">Next</button>
-                             
-      <script src="app.js"></script>                       
-  </body>                             
+<div class="gallery">
+  <a target="_blank" href="images/lamp.jpg">
+    <img src="images/lamp.jpg" alt="Lamp" width="600" height="400">
+  </a>
+  <div class="desc">Artwork made by Ben Kallman</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/dragon.jpg">
+    <img src="images/dragon.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Artwork made by Ben Kallman</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/dadart(1).jpg">
+    <img src="images/dadart(1).jpg" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Artwork made by Ben Kallman</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/ninja.jpg">
+    <img src="images/ninja.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Artwork made by Ben Kallman</div>
+</div>
+
+</body>
+</html>                     
